2ff0bef0741ba65c3fe1bbfbfa45cca5cce6420d
z1mo0x <droweckiy@mail.ru>
2026-02-08 12:50:52 +0300

    first

--- js/main.js
+++ js/main.js
@@ -116,5 +116,93 @@ document.addEventListener('DOMContentLoaded', () => {
 
     questionsItems()
 
+
+
+    const observer = new IntersectionObserver((entries) => {
+        entries.forEach((entry) => {
+            if (entry.isIntersecting) {
+                entry.target.classList.add('visible')
+            }
+        })
+    }, { threshold: .5 })
+
+
+
+    const items = document.querySelectorAll('.animation')
+
+    items.forEach((item) => {
+        observer.observe(item)
+    })
+
+
+    const inputs = document.querySelectorAll("input,textarea");
+
+    inputs.forEach((input) => {
+        input.addEventListener('change', () => {
+            if (input.value.trim() !== '') {
+                input.classList.add('no-empty');
+                console.log(input.name);
+
+            } else {
+                input.classList.remove('no-empty');
+            }
+        })
+    })
+
+
+    // маска телефона
+
+    var inputElements = document.querySelectorAll(".maskphone");
+
+    for (let inputElement of inputElements) {
+        console.log(inputElement);
+
+        inputElement.addEventListener("input", mask);
+        inputElement.addEventListener("focus", mask);
+        inputElement.addEventListener("blur", mask);
+
+        function mask(event) {
+            var blank = "+_ (___) ___-__-__";
+
+            var i = 0;
+            var val = this.value.replace(/\D/g, "").replace(/^8/, "7");
+            // <---
+
+            this.value = blank.replace(/./g, function (char) {
+                if (/[_\d]/.test(char) && i < val.length)
+                    return val.charAt(i++);
+
+                return i >= val.length ? "" : char;
+            });
+
+            if (event.type == "blur") {
+                if (this.value.length == 2)
+                    this.value = "";
+            } else {
+                setCursorPosition(this, this.value.length);
+            }
+        }
+        ;
+        function setCursorPosition(elem, pos) {
+            elem.focus();
+
+            if (elem.setSelectionRange) {
+                elem.setSelectionRange(pos, pos);
+                return;
+            }
+
+            if (elem.createTextRange) {
+                var range = elem.createTextRange();
+                range.collapse(true);
+                range.moveEnd("character", pos);
+                range.moveStart("character", pos);
+                range.select();
+                return;
+            }
+        }
+    }
+
+    // маска телефона
+
 })
 
